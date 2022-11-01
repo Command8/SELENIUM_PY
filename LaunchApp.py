5d6e75a472d51b708cbcf8934967c0e80f2d77b2
Command8 <106775101+Command8@users.noreply.github.com>
2022-11-01 15:45:44 -0400

    Update LaunchApp.py
    removed urls and usernames

--- LaunchApp.py
+++ LaunchApp.py
@@ -11,11 +11,11 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 
-#serv_obj = Service("C:\Users\kkandikatla\Documents\Drivers\chromedriver_Win32\chromedriver.exe")
-#serv_obj = Service("C:/Users/kkandikatla/chromedriver.exe")
+#serv_obj = Service("C:\Users\<username>\Documents\Drivers\chromedriver_Win32\chromedriver.exe")
+#serv_obj = Service("C:/Users/<username>/chromedriver.exe")
 serv_obj = Service('C:\Program Files (x86)\chromedriver.exe')
 driver = webdriver.Chrome(service=serv_obj)
-driver.get("https://cures.azurewebsites.net/")
+driver.get("https://<application url/")
 driver.maximize_window()
 l = driver.find_element(By.XPATH,'//button[text()=\"Login\"]')
 #perform click

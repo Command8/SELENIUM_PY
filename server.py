c0e137d96db9bd000279f292cf69914980e13c3a
Command8 <106775101+Command8@users.noreply.github.com>
2022-11-01 15:47:25 -0400

    Update server.py
    Removed user names and urls

--- server.py
+++ server.py
@@ -2,9 +2,9 @@ from webbrowser import Chrome
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-#serv_obj = Service("C:\Users\kkandikatla\Documents\Drivers\chromedriver_Win32\chromedriver.exe")
-serv_obj = Service("C:\Users\anrosario\Downloads\chromedriver.exe")
+#serv_obj = Service("C:\Users\<username>\Documents\Drivers\chromedriver_Win32\chromedriver.exe")
+serv_obj = Service("C:\Users\<username>\Downloads\chromedriver.exe")
 driver = webdriver.Chrome(service=serv_obj)
-driver.get("https://cures.azurewebsites.net/")
+driver.get("https://<applicaton url>/")
 driver.maximize_window()
 driver.close()
